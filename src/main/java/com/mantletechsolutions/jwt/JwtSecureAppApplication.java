417504cdd00e2b39ec6dfd50a903ca5845cddd5f
akanimo sam okure <akan.okure@gmail.com>
2026-03-19 15:01:53 +0000

    initail setup

---
+++ src/main/java/com/mantletechsolutions/jwt/JwtSecureAppApplication.java
@@ -0,0 +1,13 @@
+package com.mantletechsolutions.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecureAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSecureAppApplication.class, args);
+	}
+
+}
